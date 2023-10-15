467492babc5521892e4119d065cfa462e27d03f8
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-15 18:28:01 +0800

    Solve 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempF) {
+  if (typeof(tempF) !== "number") return "ERROR";
+
+  const FORMULA = (tempF - 32.0) * 5.0/9.0;
+
+  let tempC = Math.round(FORMULA * 10.0) / 10.0;
+  tempC = tempC.toFixed(1);
+  tempC = parseFloat(tempC);
+
+  return tempC;
 };
 
-const convertToFahrenheit = function() {
+// tempF = tempC * (9/5) +32
+
+const convertToFahrenheit = function(tempC) {
+  if (typeof(tempC) !== "number") return "ERROR";
+
+  const FORMULA = (tempC * 9.0/5.0) + 32.0;
+
+  let tempF = Math.round(FORMULA * 10.0) / 10.0;
+  tempF = tempF.toFixed(1);
+  tempF = parseFloat(tempF);
+
+  return tempF;
 };
 
 // Do not edit below this line
