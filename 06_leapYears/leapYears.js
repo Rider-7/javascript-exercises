384a240a8afc44f2ec396f364bbe6a5e618b7967
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-15 17:54:35 +0800

    Refactor 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -2,20 +2,12 @@ const leapYears = function(year) {
     if (!Number.isSafeInteger(year)) return "ERROR";
     if (year < 0) return "ERROR";
 
-    if ((year % 4 === 0)) {
-        if (year % 100 === 0){
-            if (year % 400 === 0) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return true;
-        }
-    }  else {
-        return false;
+    if (year % 4 === 0) {
+        if (year % 400 === 0) return true;
+        if (year % 100 === 0) return false;
+        return true;
     }
-
+    return false;
 };
 
 // Do not edit below this line
