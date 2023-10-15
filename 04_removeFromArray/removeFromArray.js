4b541d5cf2f5112d34cabd9027e4e3b208bb6789
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-15 16:20:44 +0800

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...elements) {
+    let removedArray;
+    for (let i = 0; i < elements.length; i++) {
+            removedArray = array.filter((el) => !elements.includes(el));
+        }
+    return removedArray; 
+    }
 
 // Do not edit below this line
 module.exports = removeFromArray;
