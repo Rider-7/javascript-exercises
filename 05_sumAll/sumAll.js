6fb2eb68fa3ff70f679ed4c8209a5f90d11cdb33
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-15 16:45:06 +0800

    Solve 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
 
+    if (!Number.isSafeInteger(a) || !Number.isSafeInteger(b)) return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+
+    if (a === b) return a+b;
+    let start;
+    let stop;
+    if (a < b) {
+        start = a;
+        stop = b;
+    }
+    if (a > b) {
+        start = b;
+        stop = a;
+    }
+
+    let sum = 0;
+    for(let i = start; i <= stop; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
