ceb5c13444a7d5613baf1ec747e4bb0a794cf519
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-15 15:17:19 +0800

    Solve 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) return "ERROR";
+
+    let conString = "";
+    for (let i = 0; i < num; i++) {
+        conString += string;
+    }
+    return conString;
 };
 
 // Do not edit below this line
