40bcb48387d2eccc655ec7b91f6b6ac2c7565cc7
Rider-7 <147482241+Rider-7@users.noreply.github.com>
2023-10-15 17:49:06 +0800

    Solve 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isSafeInteger(year)) return "ERROR";
+    if (year < 0) return "ERROR";
+
+    if ((year % 4 === 0)) {
+        if (year % 100 === 0){
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }  else {
+        return false;
+    }
 
 };
 
